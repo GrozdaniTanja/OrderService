c0887c203c53425509a7e3b6e46dc7e8ea2e8896
Tanja <tanja.grozdani@student.um.si>
2024-03-23 13:06:23 +0100

    add unit tests

--- src/main/java/com/tanja/order_service/dao/OrderRepository.java
+++ src/main/java/com/tanja/order_service/dao/OrderRepository.java
@@ -10,7 +10,6 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
 import java.util.List;
 
 @ApplicationScoped
--- src/test/java/com/tanja/order_service/OrderResourceTest.java
+++ src/test/java/com/tanja/order_service/OrderResourceTest.java
@@ -1,20 +1,109 @@
 package com.tanja.order_service;
 
+import com.tanja.order_service.dao.OrderRepository;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.smallrye.mutiny.Uni;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+;
 
 @QuarkusTest
-class OrderResourceTest {
+public class OrderResourceTest {
+    private String orderId;
+
+    @InjectMock
+    OrderRepository orderRepository;
+
     @Test
-    void testHelloEndpoint() {
+    void testCreateOrder() {
+        Mockito.when(orderRepository.createOrder(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyDouble())).thenReturn(Uni.createFrom().nullItem());
+
+
+        // Make the POST request to the endpoint
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .contentType(ContentType.JSON)
+                .body("{\"orderNumber\": \"Test OrderNumber\", \"userId\": \"Test UserId\", \"productId\": \"Test ProductId\", \"totalCost\": 30.5}")
+                .when()
+                .post("/order")
+                .then()
+                .statusCode(204); // Assert that the endpoint returns a 204 status code
+
+        // Optionally, verify that the createOrder method was called with the correct arguments
+        Mockito.verify(orderRepository).createOrder(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyDouble());
     }
 
+    @Test
+    void testGetOrderById_OrderFound() {
+        JsonObject expectedOrderJson = Json.createObjectBuilder()
+                .add("id", "65fea17fa9d694237d920f25")
+                .add("orderNumber", "Test OrderNumber")
+                .add("userId", "Test UserId")
+                .add("productId", "Test ProductId")
+                .add("totalCost", 30.5)
+                .build();
+
+        Mockito.when(orderRepository.getOrderById(ArgumentMatchers.anyString())).thenReturn(Uni.createFrom().item(Response.ok(expectedOrderJson).build()));
+
+        // Make the GET request to the endpoint
+        given()
+                .when()
+                .get("/order/65fea17fa9d694237d920f25")
+                .then()
+                .statusCode(200); // Assert that the endpoint returns a 200 status code
+
+        // Optionally, verify that the getOrderById method was called with the correct arguments
+        Mockito.verify(orderRepository).getOrderById("65fea17fa9d694237d920f25");
+    }
+
+    @Test
+    void testGetAllOrdersEndpoint() {
+        given()
+                .when().get("/order")
+                .then()
+                .statusCode(200); // Assuming successful retrieval returns 200
+    }
+
+
+
+    @Test
+    void testUpdateOrder_Successful() {
+        // Stub the behavior of the updateOrder method to return a successful result
+        Mockito.when(orderRepository.updateOrder(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyDouble())).thenReturn(Uni.createFrom().nullItem());
+        // Make the PUT request to the endpoint
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"orderNumber\": \"Updated OrderNumber\", \"userId\": \"Updated UserId\", \"productId\": \"Updated ProductId\", \"totalCost\": 40.0}")
+                .when()
+                .put("/order/65fea17fa9d694237d920f25")
+                .then()
+                .statusCode(204); // Assert that the endpoint returns a 204 status code
+
+        // Optionally, verify that the updateOrder method was called with the correct arguments
+        Mockito.verify(orderRepository).updateOrder("65fea17fa9d694237d920f25", "Updated OrderNumber", "Updated UserId", "Updated ProductId", 40.0);
+    }
+
+    @Test
+    void testDeleteOrder_Successful() {
+        Mockito.when(orderRepository.deleteOrder(Mockito.anyString())).thenReturn(Uni.createFrom().nullItem());
+
+
+        // Make the DELETE request to the endpoint
+        given()
+                .when()
+                .delete("/order/65fea17fa9d694237d920f25")
+                .then()
+                .statusCode(204); // Assert that the endpoint returns a 204 status code
+
+        // Optionally, verify that the deleteOrder method was called with the correct arguments
+        Mockito.verify(orderRepository).deleteOrder("65fea17fa9d694237d920f25");
+    }
 }
